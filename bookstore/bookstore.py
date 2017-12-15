1cbdd8822f8449709625fa46bd67d05b507207b4
Matthew Wilhite <mwilhite2316@gmail.com>
2017-12-15 04:11:51 +0000

    I actually understand OOP now

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,10 +1,45 @@
 class Bookstore(object):
-    pass
+    def __init__(self, name):
+        self.name = name
+        self.authors = []
+        self.books = []
+        
+    def get_books(self):
+        return self.books
+        
+    def add_book(self, book):
+        self.books.append(book)
+        
+    def search_books(self, title='', author=''):
+        books = []
+        for book in self.books:
+            if title != '':
+                if title.lower() in book.title.lower():
+                    books.append(book)
+            elif author != '':
+                if book.author == author:
+                    books.append(book)
+        return books
+        
 
 
 class Author(object):
-    pass
-
-
+    def __init__(self, name, nationality):
+        self.name = name
+        self.nationality = nationality
+        self.books = []
+        
+    def get_books(self):
+        return self.books
+        
+        
+        
 class Book(object):
-    pass
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        author.books.append(self)
+        
+   
+        
+    
